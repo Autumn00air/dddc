b2f48c6ed9cc4c9c65dd8fe428ea32f3eb12609d
fy <2323579936@qq.com>
2023-11-22 14:48:10 +0800

    ss

---
+++ src/views/sandbox/NewsSandBox.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import SideMenu from '../../components/sandbox/SideMenu'
+import TopHeader from '../../components/sandbox/TopHeader'
+import { Route, Switch,Redirect } from 'react-router-dom'
+import Home from './home/Home'
+import UserList from './user-manage/UserList'
+import RoleList from './right-manage/RoleList'
+import RightList from './right-manage/RightList'
+import Nopermission from './nopermission/Nopermission'
+
+export default function NewsSandBox() {
+  return (
+    <div>
+      <SideMenu></SideMenu>
+      <TopHeader></TopHeader>
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/user-manager/list" component={UserList} />
+        <Route path="/right-manage/role/list" component={RoleList} />
+        <Route path="/right-manage/right/list" component={RightList} />
+        <Redirect from="/" to="/home" exact/>
+        <Route path="*" component={Nopermission}/>
+      </Switch>
+    </div>
+  )
+}
